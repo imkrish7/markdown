0cb277a8b5807dac191fad7704e6b9775dfc2c04
imkrish07 <singhkrishk07@gmail.com>
2019-04-24 09:32:29 +0530

    complete

---
+++ src/preview.js
@@ -0,0 +1,87 @@
+import React, { Component } from 'react';
+const marked = require('marked')
+
+class Previewer extends Component{
+   constructor(props){
+       super(props);
+      this.state={
+           markdown: `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+  
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+  
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want! 
+1. But the list goes on...
+- Even if you use dashes or asterisks.
+* And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://goo.gl/Umyytc)
+`
+       }
+       this.handleChange=this.handleChange.bind(this);
+   }
+   handleChange(event){
+       this.setState({
+           markdown:event.target.value
+       })
+   }  
+  
+    render(){
+
+        marked.setOptions({
+            breaks: true,
+            gfm: true
+        });
+        
+        const renderer = new marked.Renderer();
+        renderer.link = function (href, title, text) {
+            return `<a target="_blank" href="${href}">${text}` + '</a>';
+        }
+
+        return (        
+            <div className="main">
+            <h3 className="editor_head">Editer</h3>
+            <h3 className="preview_head">Previewer</h3> 
+            <textarea id='editer' value={this.state.markdown} onChange={this.handleChange}></textarea>
+            <div id='preview' dangerouslySetInnerHTML={{__html:marked(this.state.markdown,{ renderer : renderer })}}></div> 
+                
+            </div>
+        )
+    }
+}
+
+export default Previewer;
